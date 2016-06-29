09e56846bfceb6e5f17f4ba4ea7210c01027be38
z41937295 <z419379295@gmail.com>
2016-06-29 12:53:12 +0000

    mario.c completed, pass the test

---
+++ pset1/mario.c
@@ -0,0 +1,42 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print(int n, char s);
+
+
+int main(void){
+    
+    int n;
+    int i = 0, j = 0;
+    // error checking
+    do
+    {
+        printf("Height: ");
+        n = GetInt();
+    }
+    while(n<0 || n>23);
+    
+    j = n;
+    for(i = 1; i<=n; i++){
+        print(j-1,'b');
+        print(i+1,'h');
+        printf("\n");
+        j--;
+    }
+    
+    return 0;
+}
+
+void print(int n, char s){
+    int i = 0;
+    for(i = 0; i<n; i++){
+        // print n hash symbols
+        if (s == 'h'){
+            printf("#");
+        }
+        // print n blank space symbols
+        else if(s == 'b'){
+            printf(" ");
+        }
+    }
+}
