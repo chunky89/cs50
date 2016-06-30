8397ad85a7e86aa26f3755292a82d3033df7c702
z41937295 <z419379295@gmail.com>
2016-06-30 08:09:37 +0000

    mario.c and credit.c waits for passing the test

---
+++ pset1hacker/credit.c
@@ -0,0 +1,140 @@
+#include <cs50.h>
+#include <stdio.h>
+
+#define MAX_CARD_LENGTH 16
+#define VALID 1
+#define NOT_VALID 0
+
+void printArray(int *, int length);
+//check whether a card number is valid
+int validCheck(int *);
+
+int main(void){
+    
+    int digit[MAX_CARD_LENGTH] = {0};
+    long long cardNumber;
+    int i; // array index
+    
+    // error checking and user input
+    do{
+        printf("Number: ");
+        cardNumber = GetLongLong();
+    }
+    while(cardNumber<=0);
+    
+    // decompose number into digits, and store it in an array
+    for(i = MAX_CARD_LENGTH; cardNumber>0; i--){
+        digit[i-1] = cardNumber % 10;    
+        cardNumber /= 10;
+    }
+    // debugging
+    //printf("i = %d\n", i);
+    //printArray(digit, MAX_CARD_LENGTH);
+    
+    /* visa 13 or 16 digits
+       AMEX 15 digits
+       MasterCard 16 digits 
+    */
+    // checking if the last digits is zero(valid) and it starts with digit 4
+    if(validCheck(digit) == VALID){
+        // 13 digits starts with 4 (must be visa)
+        if(digit[0] == 0 && digit[1] == 0 && digit[2] == 0 && digit[3] == 4){
+            printf("VISA\n");
+        }
+        // 15 digits starts with 34 or 37 (must be AMEX)
+        else if(digit[0] == 0 && digit[1]  == 3 && (digit[2] == 4 || digit[2] == 7)){
+            printf("AMEX\n");    
+        }
+        // 16 digits (could be MasterCard or Visa)
+        else if(digit[0]!=0){
+            if(digit[0] == 4){
+                printf("VISA\n");
+            }
+            else if(digit[0] == 5 && (digit[1] == 1 || digit[1] == 2 || digit[1] == 3 || digit[1] == 4 || digit[1]==5)){
+                printf("MASTERCARD\n");
+            }
+        }
+    }
+    else {
+        printf("INVALID\n");
+    }
+    
+    return 0;
+}
+
+int validCheck(int *set){
+    int even_digit_sum = 0;
+    int odd_digit_sum = 0;
+    int j = 0; // array index 
+    int k = 0; // array index
+    
+    //probably a visa card with 13 digits (note: visa can also be 16 digits)
+    if(set[0] == 0 && set[1] == 0 && set[2] == 0 && set[3] != 0){
+        for(j = 4; j<= MAX_CARD_LENGTH-1; j+=2){
+            // the product is double digit, decompose it to two single digits
+            if(set[j]*2 - 10 >= 0){
+                even_digit_sum += (set[j]*2-10) + 1;            
+            }
+            else {
+                even_digit_sum += set[j]*2;
+            }
+        }
+        
+        for(k = 3;k<=MAX_CARD_LENGTH-1;k+=2){
+            odd_digit_sum += set[k];
+        }
+    }
+    //probably an AMEX card with 15 digits
+    else if(set[0] == 0 && set[1] != 0){
+        for(j = 2; j<= MAX_CARD_LENGTH-1; j+=2){
+            // the product is double digit, decompose it to two single digits
+            if(set[j]*2 - 10 >= 0){
+                even_digit_sum += (set[j]*2-10) + 1;
+                //printf("inside double digit, even sum = %d\n", even_digit_sum);
+            }
+            else{
+                even_digit_sum += set[j]*2;
+            }
+            //printf("even sum = %d\n", even_digit_sum);
+        }
+        for(k = 1;k<=MAX_CARD_LENGTH-1;k+=2){
+            odd_digit_sum += set[k];
+        } 
+        //printf("odd sum = %d\n", odd_digit_sum);
+    }
+    //probably a MasterCard or Visa card with 16 digits
+    else if(set[0]!= 0){
+        for(j = 0; j<= MAX_CARD_LENGTH-1; j+=2){
+            // the product is double digit, decompose it to two single digits
+            if(set[j]*2 - 10 >= 0){
+                even_digit_sum += (set[j]*2-10) + 1;
+                //printf("inside double digit, even sum = %d\n", even_digit_sum);
+            }
+            else{
+                even_digit_sum += set[j]*2;
+            }
+            //printf("even sum = %d\n", even_digit_sum);
+        }
+        for(k = 1;k<=MAX_CARD_LENGTH-1;k+=2){
+            odd_digit_sum += set[k];
+        } 
+        //printf("odd sum = %d\n", odd_digit_sum);        
+    }
+    
+    if((even_digit_sum + odd_digit_sum) % 10 == 0){
+        return VALID;
+    }
+    else {
+        return NOT_VALID;
+    }
+}
+
+// debugging
+void printArray(int *set, int length){
+    int i;
+    
+    for(i = 0; i<length; i++){
+        printf("%d ", set[i]);
+    }
+    printf("\n");
+}
---
+++ pset1hacker/mario.c
@@ -0,0 +1,46 @@
+#include <cs50.h>
+#include <stdio.h>
+
+#define GAP 2
+
+void print(int n, char s);
+
+int main(void){
+    
+    int n;
+    int i = 0, j = 0;
+    // error checking
+    do
+    {
+        printf("Height: ");
+        n = GetInt();
+    }
+    while(n<0 || n>23);
+    
+    j = n;
+    for(i = 1; i<=n; i++){
+        print(j-1,'b');
+        print(i+1,'h');
+        print(GAP, 'b');
+        print(i+1,'h');
+        print(j-1,'b');
+        printf("\n");
+        j--;
+    }
+    
+    return 0;
+}
+
+void print(int n, char s){
+    int i = 0;
+    for(i = 0; i<n; i++){
+        // print n hash symbols
+        if (s == 'h'){
+            printf("#");
+        }
+        // print n blank space symbols
+        else if(s == 'b'){
+            printf(" ");
+        }
+    }
+}
