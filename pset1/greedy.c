2f8d09cedb57d1ab6623610401ba290d3746f4b3
z41937295 <z419379295@gmail.com>
2016-07-01 05:31:08 +0000

    greedy.c pass the test

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -1,10 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
-#define QUARTER 0.25
-#define DIME    0.10
-#define NICKEL  0.05
-#define PENNY   0.01
+#define QUARTER 25
+#define DIME    10
+#define NICKEL  5
+#define PENNY   1
 
 
 int main(void){
@@ -24,34 +25,45 @@ int main(void){
     }
     while(n<0);
     
+    int change_in_cents = round((100*(float)n));
+    // debugging
+    //printf("n in float = %f\n", n*100);
+    //printf("before main logic, n = %d\n", change_in_cents);
+    
     // main logic
-    while(n >=QUARTER && n <= n){
-        n -= QUARTER;
+    while(change_in_cents >=QUARTER){
+        change_in_cents -= QUARTER;
         numberOfQuarters +=1;
     }
+    //printf("n<input, n = %d\n", change_in_cents);
     
-    while(n >= DIME && n <= QUARTER){
-        n -= DIME;
+    while(change_in_cents >= DIME && change_in_cents < QUARTER){
+        change_in_cents -= DIME;
         numberOfDimes +=1;
     }
+    //printf("n<QUARTER, n = %d\n", change_in_cents);
     
-    while(n >= NICKEL && n <= DIME){
-        n -= NICKEL;
+    while(change_in_cents >= NICKEL && n < DIME){
+        change_in_cents -= NICKEL;
         numberOfNickels +=1;
     }
+    //printf("n<DIME, n = %d\n", change_in_cents);
     
-    while(n >= PENNY && n <= NICKEL){
-        n -= PENNY;
+    while(change_in_cents >= PENNY && change_in_cents < NICKEL){
+        change_in_cents -= PENNY;
         numberOfPennies +=1;
     }
+    //printf("n<NICKEL, n = %d\n", change_in_cents);
     
-    while(n>0){
-        n -= PENNY;
+    while(change_in_cents > 0){
+        change_in_cents -= PENNY;
         numberOfPennies +=1;
+        printf("11111\n");
     }
+    //printf("n>0, n = %d\n", change_in_cents);
     
     /* debugging
-    printf("the balance is now: %f\n", n);
+    printf("the balance is now: %d\n", change_in_cents);
     printf("number of quarters is %d\n", numberOfQuarters);
     printf("number of dimes is %d\n", numberOfDimes);
     printf("number of nickels is %d\n", numberOfNickels);
