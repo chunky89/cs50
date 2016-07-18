8e714e04c3995bf9a9e7b83e0e7cef656af6fe97
z419379295 <z419379295@gmail.com>
2016-07-18 09:01:43 +0000

    reorganize the files

--- pset4/bmp/resize/resize.c
+++ pset4/bmp/resize/resize.c
@@ -54,6 +54,7 @@ int main(int argc, char* argv[])
     // get a copy of original info header
     BITMAPINFOHEADER new_bi = bi;
     
+    /*
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
         bi.biBitCount != 24 || bi.biCompression != 0)
@@ -63,16 +64,21 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Unsupported file format.\n");
         return 4;
     }
+    */
 
     // determine padding for scanlines
     int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
     int new_padding =  (4 - (bi.biWidth * factor * sizeof(RGBTRIPLE)) % 4) % 4;
-    RGBTRIPLE temp[bi.biWidth*factor + new_padding];
+    //RGBTRIPLE temp[bi.biWidth*factor + new_padding];
+    int index = 0;
     
     // iterate over infile's scanlines
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
     {
+        RGBTRIPLE *temp = malloc(sizeof(RGBTRIPLE)*(bi.biWidth*factor + new_padding));
+        
         // iterate over pixels in scanline
+        index = 0;
         for (int j = 0; j < bi.biWidth; j++)
         {
             // temporary storage
@@ -83,7 +89,8 @@ int main(int argc, char* argv[])
             for(int k = 0; k < factor; k++){
                 // write RGB triple to outfile
                 fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-                temp[k] = triple;
+                temp[index] = triple;
+                index++;
             }
         }
 
@@ -93,8 +100,17 @@ int main(int argc, char* argv[])
         // then add it back (to demonstrate how)
         for (int k = 0; k < new_padding; k++)
         {
+            temp[index].rgbtBlue = 0x00;
+            temp[index].rgbtGreen = 0x00;
+            temp[index].rgbtRed = 0x00;
             fputc(0x00, outptr);
+            index++;
         }
+        
+        for(int g = 0; g < factor; g++){
+            fwrite(&temp, sizeof(RGBTRIPLE), 1, outptr);
+        }
+        free(temp);
     }
     
     /**
@@ -104,12 +120,16 @@ int main(int argc, char* argv[])
      * 3. biHeight: new height
      * 4. biSizeImage: new width * new height * biBicount(3 bytes per pixel)
     **/
+    
+    new_bi.biWidth = bi.biWidth * factor;
+    new_bi.biHeight = bi.biHeight * factor;
+    new_bi.biSizeImage = new_bi.biWidth * new_bi.biHeight * new_bi.biBitCount + new_padding * new_bi.biWidth;
+    new_bf.bfSize = 14 + 40 + new_bi.biSizeImage;
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&new_bi, sizeof(BITMAPINFOHEADER), 1, outptr);
     // write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
-
-    // write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
-
+    
     // close infile
     fclose(inptr);
 
