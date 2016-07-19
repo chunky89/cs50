d1baf1f377b5451bf7233c3c200409dd390c9a72
z419379295 <z419379295@gmail.com>
2016-07-19 14:57:43 +0000

    resize.c finished, pass the test

--- pset4/bmp/resize/resize.c
+++ pset4/bmp/resize/resize.c
@@ -23,6 +23,9 @@ int main(int argc, char* argv[])
 
     // remember filenames
     int factor = atoi(argv[1]);
+    if(factor < 1 || factor > 100)
+        return 1;
+    
     char* infile = argv[2];
     char* outfile = argv[3];
 
@@ -54,7 +57,7 @@ int main(int argc, char* argv[])
     // get a copy of original info header
     BITMAPINFOHEADER new_bi = bi;
     
-    /*
+    
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
         bi.biBitCount != 24 || bi.biCompression != 0)
@@ -64,71 +67,71 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Unsupported file format.\n");
         return 4;
     }
-    */
+    
 
-    // determine padding for scanlines
+    /**
+     * need to write four things:
+     * 1. bfSize
+     * 2. biWidth: new width
+     * 3. biHeight: new height
+     * 4. biSizeImage
+    **/
+    
+    // // determine padding for scanlines
     int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
     int new_padding =  (4 - (bi.biWidth * factor * sizeof(RGBTRIPLE)) % 4) % 4;
-    //RGBTRIPLE temp[bi.biWidth*factor + new_padding];
-    int index = 0;
+    
+    new_bi.biWidth = bi.biWidth * factor;
+    new_bi.biHeight = bi.biHeight * factor;
+    new_bi.biSizeImage = (sizeof(RGBTRIPLE)*new_bi.biWidth + new_padding)*abs(new_bi.biHeight);
+    new_bf.bfSize = bf.bfSize - bi.biSizeImage + new_bi.biSizeImage;
+    
+    /**
+     * Must write the file header first, then info header, 
+     * because it is how they are located in bmp file. Previous
+     * mistake, write it at the end of the main function.
+    **/
+    
+    // write outfile's BITMAPFILEHEADER
+    fwrite(&new_bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&new_bi, sizeof(BITMAPINFOHEADER), 1, outptr);
     
     // iterate over infile's scanlines
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
     {
-        RGBTRIPLE *temp = malloc(sizeof(RGBTRIPLE)*(bi.biWidth*factor + new_padding));
         
-        // iterate over pixels in scanline
-        index = 0;
-        for (int j = 0; j < bi.biWidth; j++)
+        for(int j = 0; j < factor; j++)
         {
-            // temporary storage
-            RGBTRIPLE triple;
-
-            // read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
-            for(int k = 0; k < factor; k++){
-                // write RGB triple to outfile
-                fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-                temp[index] = triple;
-                index++;
+            // iterate over pixels in scanline
+            for (int k = 0; k < bi.biWidth; k++)
+            {
+                // temporary storage
+                RGBTRIPLE triple;
+                // read RGB triple from infile
+                fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+                for(int l = 0; l < factor; l++){
+                    // write RGB triple to outfile
+                    fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+                }
             }
-        }
-
-        // skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
-
-        // then add it back (to demonstrate how)
-        for (int k = 0; k < new_padding; k++)
-        {
-            temp[index].rgbtBlue = 0x00;
-            temp[index].rgbtGreen = 0x00;
-            temp[index].rgbtRed = 0x00;
-            fputc(0x00, outptr);
-            index++;
+    
+            // skip over padding, if any
+            fseek(inptr, padding, SEEK_CUR);
+    
+            // then add it back (to demonstrate how)
+            for (int k = 0; k < new_padding; k++)
+            {
+                fputc(0x00, outptr);
+            }
+            
+            // move the cursor back to the beginning of the scanline
+            if (j < factor - 1)
+                // only move the cursor factor -1 times
+                fseek(inptr,-(padding + sizeof(RGBTRIPLE)*bi.biWidth), SEEK_CUR);
         }
         
-        for(int g = 0; g < factor; g++){
-            fwrite(&temp, sizeof(RGBTRIPLE), 1, outptr);
-        }
-        free(temp);
     }
-    
-    /**
-     * need to write four things:
-     * 1. bfSize: length of headers(file header 14 bytes, info header 40 bytes) + biSizeImage(image size + padding)
-     * 2. biWidth: new width
-     * 3. biHeight: new height
-     * 4. biSizeImage: new width * new height * biBicount(3 bytes per pixel)
-    **/
-    
-    new_bi.biWidth = bi.biWidth * factor;
-    new_bi.biHeight = bi.biHeight * factor;
-    new_bi.biSizeImage = new_bi.biWidth * new_bi.biHeight * new_bi.biBitCount + new_padding * new_bi.biWidth;
-    new_bf.bfSize = 14 + 40 + new_bi.biSizeImage;
-    // write outfile's BITMAPINFOHEADER
-    fwrite(&new_bi, sizeof(BITMAPINFOHEADER), 1, outptr);
-    // write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
     
     // close infile
     fclose(inptr);
