f317c2ff46e70a52d73f3469f56140606d5bf2d6
z419379295 <z419379295@gmail.com>
2016-07-26 07:29:10 +0000

    data structure created, need to load the words from dictionary to the data structure

--- pset5/dictionary.c
+++ pset5/dictionary.c
@@ -8,6 +8,7 @@
  */
 
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "dictionary.h"
 
@@ -26,6 +27,24 @@ bool check(const char* word)
 bool load(const char* dictionary)
 {
     // TODO
+    /**
+     * 0. create a dictionary
+     * 1. declare a file pointer and open the dictionary
+     * 2. scan every word in the dictionary, load it to the data structure
+     * 3. close the file
+     * 4. return true
+    **/
+    
+    dict d = createDictionary(); 
+    FILE *fp = fopen(dictionary, "r");
+    assert(fp != NULL);
+    // temporary storage
+    char x[LENGTH+1];
+    /* assumes no word exceeds length of 45 */
+    while (fscanf(fp, " %1023s", x) == 1) {
+        // load it to the data structure
+    }
+
     return false;
 }
 
@@ -45,4 +64,66 @@ bool unload(void)
 {
     // TODO
     return false;
+}
+
+int hash(int letter)
+{
+    return letter - 'a';    
+}
+
+link create_node(const char *word)
+{
+    // this function accepts a word and create a node with this word
+    link newNode = malloc(sizeof(struct node));
+    assert(newNode != NULL);
+    
+    strcpy(newNode->word, word);
+    newNode->next = NULL;
+    
+    return newNode;
+}
+
+link insertNode(link head, link node)
+{
+    // node->next = head;
+    // head = node;
+    
+    // return head;
+    
+    link next_node = head;
+    link prev_node = NULL;
+    
+    while(next_node && strcmp(node->word, next_node->word) >0) 
+    {
+        prev_node = next_node;
+        next_node = next_node->next; // find correct position
+    }
+    
+    if(prev_node == NULL)
+    {
+        head = node;
+    }
+    else 
+    {
+        prev_node->next = node; // link inserting node into list 
+    }
+    node->next = next_node;
+    
+    return head;
+}
+
+dict createDictionary(void)
+{
+    // allocate memory for an array of linked list
+    dict newDict = malloc(NUMBER_OF_ALPHABETS * sizeof(struct dictNode));
+    assert(newDict != NULL);
+    
+    // initialize the node pointer and size
+    for(int i = 0; i<NUMBER_OF_ALPHABETS; i++)
+    {
+        newDict[i].letter = NULL;
+        newDict[i].n = 0;
+    }
+    
+    return newDict;
 }
--- pset5/dictionary.h
+++ pset5/dictionary.h
@@ -11,10 +11,38 @@
 #define DICTIONARY_H
 
 #include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 
 // maximum length for a word
 // (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
 #define LENGTH 45
+#define NUMBER_OF_ALPHABETS 26
+
+/**
+ * Data structure of the linked list
+**/
+typedef struct node* link;
+struct node
+{
+    char word[LENGTH+1];
+    link next;
+};
+
+/**
+ * Data structure of the dictionary - an array of linked list
+ 
+ * For each dictionary node, it contains the number of words belongs
+ * to a particular buckets and a linked list
+**/
+typedef struct dictNode* dict;
+struct dictNode
+{
+    int n;  // keep track of number of words
+    link letter;  // an array of linked list
+};
+
 
 /**
  * Returns true if word is in dictionary else false.
@@ -35,5 +63,26 @@ unsigned int size(void);
  * Unloads dictionary from memory.  Returns true if successful else false.
  */
 bool unload(void);
+
+/**
+ * Generate corresponding index for a letter, return 0 if the letter is 'a'
+ * return 1 if the letter is 'b' and so on.
+**/
+int hash(int letter);
+
+/**
+ * Create a new node containing a word. Return a pointer to this node.
+**/
+link createNode(const char *word);
+
+/**
+ * Insert the node to the head of the linked list. Return the new head of the linked list.
+**/
+link insertNode(link head, link node);
+
+/**
+ * Create a dictionary data structure and initialize it. Return a pointer to this dictionary. 
+**/
+dict createDictionary(void);
 
 #endif // DICTIONARY_H
